0bc8085ee06a7fc0367977dc9759e02d76a8bda9
yunpruna <146179357+yunpruna@users.noreply.github.com>
2023-10-18 09:24:20 +0200

    Actualizar pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -22,6 +22,25 @@ namespace myImages {
 9 9 9 3 3 3 f 3 f 3 3 9 9 9 9 9 
 9 9 9 3 3 3 3 3 3 3 3 9 9 9 9 9 
 `;
+            case "image2":
+            case "Hugo":return img`
+a a a a a a a a a a a a a a a a 
+a a 7 a a a a a a a a a a 7 a a 
+a 7 7 7 a a a a a a a a 7 7 7 a 
+a 7 f 7 a a a a a a a a 7 f 7 a 
+a 7 7 7 a a a a a a a a 7 7 7 a 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 4 4 4 2 2 2 2 2 2 4 4 4 2 2 
+2 2 4 5 4 2 2 2 2 2 2 4 5 4 2 2 
+2 2 4 4 4 2 2 2 2 2 2 4 4 4 2 2 
+2 2 2 2 2 2 2 6 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 6 2 6 2 2 2 2 2 2 2 
+a a 2 2 2 2 2 2 2 2 2 2 2 2 a a 
+a a 2 2 3 3 3 3 3 3 3 2 2 2 a a 
+a a 2 2 2 2 3 6 3 2 2 2 2 2 a a 
+a a 2 2 2 2 3 3 3 2 2 2 2 2 a a 
+a a 2 2 2 2 2 2 2 2 2 2 2 2 a a 
+`;
         }
         return null;
     })
